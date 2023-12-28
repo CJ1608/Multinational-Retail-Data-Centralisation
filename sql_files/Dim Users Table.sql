698668df6fc9f8545442e4b610b12e00c6c77a95
CJ1608 <128046995+CJ1608@users.noreply.github.com>
2023-12-28 17:25:31 +0000

    Update and rename T2 dim users table.sql to Dim Users Table.sql

---
+++ sql_files/Dim Users Table.sql
@@ -0,0 +1,20 @@
+-- Cast columns to correct data types
+
+ALTER TABLE dim_users
+ALTER COLUMN first_name TYPE VARCHAR(225)
+
+ALTER TABLE dim_users
+ALTER COLUMN last_name TYPE VARCHAR(225)
+
+ALTER TABLE dim_users
+ALTER COLUMN date_of_birth TYPE date
+
+ALTER TABLE dim_users
+ALTER COLUMN country_code TYPE VARCHAR(2)
+
+ALTER TABLE dim_users
+ALTER COLUMN user_uuid TYPE UUID
+USING user_uuid::UUID
+
+ALTER TABLE dim_users
+ALTER COLUMN join_date TYPE date
