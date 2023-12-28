b658bebbc3e50367efd66103391f53b094cd67b0
CJ1608 <128046995+CJ1608@users.noreply.github.com>
2023-12-28 17:37:38 +0000

    Update SQL Queries.sql

--- sql_files/SQL Queries.sql
+++ sql_files/SQL Queries.sql
@@ -1,5 +1,4 @@
---T1: how many stores does business have in each country
--- make index 0 have N/A for country code and continent in dim_store_details
+-- Find how many stores each business have in each country
 UPDATE dim_store_details
 SET "country_code" = 'N/A'
 WHERE index=0;
@@ -12,19 +11,18 @@ SELECT country_code, COUNT(*)
 FROM dim_store_details
 GROUP BY country_code
 
---T2 which locations have the most stores?
+	
+-- Find locations that have the most stores
 SELECT locality, COUNT(*)
 FROM dim_store_details
 GROUP BY locality
 ORDER BY count DESC
 LIMIT 7
 
---T3 which months produced largest number of sales
---don't need to select all columns and can do SUM in select statement
+	
+-- Find which months produced the largest number of sales
 SELECT dim_date_times.month, ROUND(CAST(SUM(orders_table.product_quantity*dim_products.product_price) AS NUMERIC), 2) AS profit
---dim_products.product_code orders_table.date_uuid,
 FROM dim_date_times
---inner join on 3 tables https://www.commandprompt.com/education/how-to-join-three-tables-in-postgresql/
 INNER JOIN orders_table 
 	ON dim_date_times.date_uuid = orders_table.date_uuid
 INNER JOIN dim_products
@@ -33,7 +31,7 @@ GROUP BY dim_date_times.month
 ORDER BY profit DESC
 
 
---T4 how many sales are from online 
+-- Find how many sales are from online 
 SELECT COUNT(orders_table.product_code) AS num_sales, SUM(orders_table.product_quantity) AS prod_count, --dim_store_details.store_type,
 CASE
 	WHEN dim_store_details.store_type ='Web Portal' THEN 'Web'
@@ -45,7 +43,7 @@ INNER JOIN dim_store_details
 GROUP BY location
 
 
---T5 what % of sales come through each store type
+-- Find what % of sales come through each store type
 SELECT dim_store_details.store_type, 
 	ROUND(CAST(SUM(orders_table.product_quantity * dim_products.product_price) AS numeric), 2) AS total_sales,
 	round(SUM(100 * orders_table.product_quantity*dim_products.product_price)/(SUM(SUM(orders_table.product_quantity*dim_products.product_price)) OVER ())) as percentage_total
@@ -58,7 +56,7 @@ GROUP BY dim_store_details.store_type
 ORDER BY percentage_total DESC
 
 
---T6 which month in EVERY year produced the highest cost of sales
+-- Find which month in every year produced the highest cost of sales
 SELECT ROUND(CAST(SUM(orders_table.product_quantity * dim_products.product_price) AS numeric), 2) as total_sales, dim_date_times.year, dim_date_times.month
 FROM orders_table
 INNER JOIN dim_date_times
@@ -70,10 +68,10 @@ order by SUM(orders_table.product_quantity * dim_products.product_price) DESC
 LIMIT 10
 
 
---T7 staff headcount 
--- ALTER TABLE dim_store_details
--- ALTER COLUMN staff_numbers TYPE bigint
--- USING staff_numbers::bigint
+-- Find the total staff headcount by country code
+ALTER TABLE dim_store_details
+ALTER COLUMN staff_numbers TYPE bigint
+USING staff_numbers::bigint
 
 SELECT sum(dim_store_details.staff_numbers) as total_staff_numbers,
 CASE
@@ -87,7 +85,7 @@ GROUP BY code
 ORDER BY total_staff_numbers desc
 
 
---T8 what german store type is selling the most
+-- Find which German store type is selling the most
 SELECT dim_store_details.store_type, dim_store_details.country_code, ROUND(CAST(SUM(orders_table.product_quantity * dim_products.product_price) AS numeric), 2) as total_sales
 FROM dim_store_details
 INNER JOIN orders_table
@@ -99,7 +97,7 @@ GROUP BY dim_store_details.store_type, dim_store_details.country_code
 ORDER BY total_sales
 
 
---T9 how quickly is company making sales
+-- Find how quickly the company is making sales
 WITH cte_timestamps AS
 (
 	SELECT year, TO_TIMESTAMP(CONCAT(dim_date_times.year, '-', month, '-', day, ' ', timestamp), 'YYYY-MM-DD H:M:S') as dt_timestamps
